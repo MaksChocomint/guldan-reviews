d106cd79a38dd46cd86f6bb286dc511135dae3f1
Maksim <maxprofix@gmail.com>
2023-08-24 11:08:34 +0300

    Update AudioPlayer.js

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MdPlayCircle, MdStopCircle } from "react-icons/md";
 
 const AudioPlayer = ({ audioUrl }) => {
@@ -13,6 +13,20 @@ const AudioPlayer = ({ audioUrl }) => {
     }
     setIsPlaying(!isPlaying);
   };
+
+  // Добавляем слушатель события 'ended' для аудио
+  useEffect(() => {
+    const handleAudioEnded = () => {
+      setIsPlaying(false); // Аудио закончилось, устанавливаем в "пауза"
+    };
+
+    audio.addEventListener("ended", handleAudioEnded);
+
+    return () => {
+      // Удаляем слушатель при размонтировании компонента
+      audio.removeEventListener("ended", handleAudioEnded);
+    };
+  }, [audio]);
 
   return (
     <div className="flex items-center text-blue-400 absolute bg-zinc-200 rounded-full top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2">
